095d1373eb51e1088ce423c4aed2754f5a8609c2
abdunnoorfaheem <abdunnoor3030@gmail.com>
2025-08-15 21:04:13 +0600

    footer section added

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Container from "../Container";
 import Flex from "../Flex";
+import {Link} from "react-router-dom";
+import Heading from "../Heading";
+import Image from "../Image";
+import Logo from "/src/assets/logo.png";
 
 const Footer = () => {
   return (
@@ -9,11 +13,65 @@ const Footer = () => {
         <Container>
         <div className="py-[30px]">
           <Flex className={"justify-between"}>
-            <div className="">1</div>
-            <div className="">2</div>
-            <div className="">3</div>
-            <div className="">4</div>
-            <div className="">5</div>
+            <div className="">
+              
+              <Heading text={"MENU"} tagName={"h3"} className={"font-bold"}/>
+               <ul className=" text-[#767676]">
+              <Link to={"/"}>
+                <li className="hover:text-[#262626] hover:font-bold duration-500 mt-[17px]">Home</li>
+              </Link>
+              <Link to={"/shop"}>
+                <li className="hover:text-[#262626] hover:font-bold duration-500">Shop</li>
+              </Link>
+              <Link to={"/about"}>
+                <li className="hover:text-[#262626] hover:font-bold duration-500">About</li>
+              </Link>
+              <li className="hover:text-[#262626] hover:font-bold duration-500"><Link to={"/contact"}>Contacts</Link></li>
+            <li className="hover:text-[#262626] hover:font-bold duration-500"><Link to={"/journal"}>Journal</Link></li>
+            </ul>
+            </div>
+            <div className="">
+              
+              <Heading text={"SHOP"} tagName={"h3"} className={"font-bold"}/>
+               <ul className=" text-[#767676]">
+              <Link to={"/"}>
+                <li className="hover:text-[#262626] hover:font-bold duration-500 mt-[17px]">Category 1</li>
+              </Link>
+              <Link to={"/shop"}>
+                <li className="hover:text-[#262626] hover:font-bold duration-500">Category 2</li>
+              </Link>
+              <Link to={"/about"}>
+                <li className="hover:text-[#262626] hover:font-bold duration-500">Category 3</li>
+              </Link>
+              <li className="hover:text-[#262626] hover:font-bold duration-500"><Link to={"/contact"}>Category 4</Link></li>
+            <li className="hover:text-[#262626] hover:font-bold duration-500"><Link to={"/journal"}>Category 5</Link></li>
+            </ul>
+            </div>
+            <div className="">
+              <Heading text={"HELP"} tagName={"h3"} className={"font-bold"}/>
+               <ul className=" text-[#767676]">
+              <Link to={"/"}>
+                <li className="hover:text-[#262626] hover:font-bold duration-500 mt-[17px]">Privacy Policy</li>
+              </Link>
+              <Link to={"/shop"}>
+                <li className="hover:text-[#262626] hover:font-bold duration-500">Terms & Conditions</li>
+              </Link>
+              <Link to={"/about"}>
+                <li className="hover:text-[#262626] hover:font-bold duration-500">Special E-shop</li>
+              </Link>
+              <li className="hover:text-[#262626] hover:font-bold duration-500"><Link to={"/contact"}>Shipping</Link></li>
+            <li className="hover:text-[#262626] hover:font-bold duration-500"><Link to={"/journal"}>Secure Payments</Link></li>
+            </ul>
+            </div>
+            
+            
+            <div className="">
+              <Heading tagName={"h4"} text={"(052) 611-5711 company@domain.com"} className={"w-[187px] font-bold leading-[27px] text-[#262626]"}/>
+              <Heading tagName={"p"} text={"575 Crescent Ave. Quakertown, PA 18951"} className={"text-[#6D6D6D] text-[14px] mt-[15px] mb-[89px]"}/>
+            </div>
+            <div className="">
+              <Image imgSrc={Logo} className={"mb-[147px]"}/>
+            </div>
           </Flex>
         </div>
       </Container>
