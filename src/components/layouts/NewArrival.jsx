47b688df0a3ecd26dac3bd0910644d1815a1bb94
abdunnoorfaheem <abdunnoor3030@gmail.com>
2025-08-19 14:44:57 +0600

    product four image changes

--- src/components/layouts/NewArrival.jsx
+++ src/components/layouts/NewArrival.jsx
@@ -6,7 +6,7 @@ import Product from "../Product";
 import ProductOne from "/src/assets/productOne.png";
 import ProductTwo from "/src/assets/productTwo.png";
 import ProductThree from "/src/assets/productThree.png";
-import ProductFour from "/src/assets/productFour.png";
+// import ProductFour from "/src/assets/productFour.png";
 
 
 const NewArrival = () => {
@@ -24,7 +24,7 @@ const NewArrival = () => {
                 <div className="w-[24%]"><Product productImg={ProductOne}  bdgText={"New"}/></div>
                 <div className="w-[24%]"><Product productImg={ProductTwo} bdgText={"20%"}/></div>
                 <div className="w-[24%]"><Product productImg={ProductThree} bdgText={"New"}/></div>
-                <div className="w-[24%]"><Product productImg={ProductFour} bdgText={"30%"}/></div>
+                <div className="w-[24%]"><Product productImg={ProductThree} bdgText={"30%"}/></div>
                 
                 
             </Flex>
