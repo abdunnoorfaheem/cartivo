6e052f2981613afb5882546c274db943f7dc8971
abdunnoorfaheem <abdunnoor3030@gmail.com>
2025-08-26 22:10:32 +0600

    bestsellers and special section api  data added

--- src/components/layouts/Bestsellers.jsx
+++ src/components/layouts/Bestsellers.jsx
@@ -3,8 +3,22 @@ import Container from "../Container";
 import Product from "../Product";
 import ProductImgFour from "/src/assets/productImgFour.png";
 import Heading from "../Heading";
+import {useState,useEffect} from "react";
+import axios from "axios"
 
 const Bestsellers = () => {
+  let [allData,setAllData]=useState([]);
+  
+  useEffect(() => {
+    
+    async function viewData(){
+      let data =await axios.get('https://dummyjson.com/product');
+      setAllData(data.data.products);
+      
+    }
+      viewData();
+  }, []);
+  
   return (
     <div className="py-[30px]">
       <Container>
@@ -13,19 +27,20 @@ const Bestsellers = () => {
           tagName={"h3"}
           className={"text-[39px] font-bold text-[#262626]"}
         />
-        <div className="flex flex-wrap gap-4 justify-between">
-          <div className="w-[48%] md:w-[32%] lg:w-[24%]">
-            <Product productTitle={"Basic Crew Neck Tee"} productImg={ProductImgFour} bdgText={"15%"}  productPrice={"$50"} />
+        <div className="flex flex-wrap gap-x-4 ">
+
+          {
+            allData.slice(0,8).map((item)=>(
+
+                <div className="w-[48%] md:w-[32%] lg:w-[24%]">
+            <Product productTitle={item.title} productImg={item.thumbnail} bdgText={"15%"}  productPrice={`$${item.price}`} />
           </div>
-          <div className="w-[48%] md:w-[32%] lg:w-[24%]">
-            <Product productTitle={"Basic Crew Neck Tee"} productImg={ProductImgFour} bdgText={"15%"}  productPrice={"$20"}/>
-          </div>
-          <div className="w-[48%] md:w-[32%] lg:w-[24%]">
-            <Product productTitle={"Basic Crew Neck Tee"} productImg={ProductImgFour} bdgText={"15%"} productPrice={"$40"} />
-          </div>
-          <div className="w-[48%] md:w-[32%] lg:w-[24%]">
-            <Product productTitle={"Basic Crew Neck Tee"} productImg={ProductImgFour} bdgText={"15%"}  productPrice={"$80"}/>
-          </div>
+            ))
+          }
+          
+        
+          
+         
         </div>
       </Container>
     </div>
--- src/components/layouts/SpecialOffer.jsx
+++ src/components/layouts/SpecialOffer.jsx
@@ -1,38 +1,55 @@
-import React from 'react';
-import Heading from '../Heading';
-import Product from '../Product';
+import React from "react";
+import Heading from "../Heading";
+import Product from "../Product";
 import SpecialImg from "/src/assets/specialProductOne.png";
 import SpecialImgTwo from "/src/assets/specialProductTwo.png";
-import Container from '../Container';
+import Container from "../Container";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const SpecialOffer = () => {
+  let [allData, setAllData] = useState([]);
+
+  useEffect(() => {
+    async function viewData() {
+      let data = await axios.get("https://dummyjson.com/products?limit=4");
+      setAllData(data.data.products);
+    }
+    viewData();
+  }, []);
   return (
     <>
       <div className="py-[30px]">
-      <Container>
-         <Heading
-          text={"Special Offers"}
-          tagName={"h3"}
-          className={"text-[39px] font-bold text-[#262626]"}
-        />
-        <div className="flex flex-wrap gap-4 justify-between">
-          <div className="w-[48%] md:w-[32%] lg:w-[24%]">
-            <Product productTitle={"Basic Crew Neck Tee"} productImg={SpecialImg} bdgText={"15%"}  productPrice={"$250"}/>
-          </div>
-          <div className="w-[48%] md:w-[32%] lg:w-[24%]">
-            <Product productTitle={"Basic Crew Neck Tee"} productImg={SpecialImg} bdgText={"15%"}  productPrice={"$50"}/>
-          </div>
-          <div className="w-[48%] md:w-[32%] lg:w-[24%]">
-            <Product productTitle={"Basic Crew Neck Tee"} productImg={SpecialImgTwo} bdgText={"15%"}  productPrice={"$30"}/>
-          </div>
-          <div className="w-[48%] md:w-[32%] lg:w-[24%]">
-            <Product productTitle={"Basic Crew Neck Tee"} productImg={SpecialImgTwo} bdgText={"15%"}  productPrice={"$10"}/>
-          </div>
-        </div>
-      </Container>
-    </div>
-    </>
-  )
-}
+        <Container>
+          <Heading
+            text={"Special Offers"}
+            tagName={"h3"}
+            className={"text-[39px] font-bold text-[#262626]"}
+          />
+          <div className="flex flex-wrap gap-4 justify-between">
 
-export default SpecialOffer
+            {
+              allData.map((item)=>(
+                  
+                <div className="w-[48%] md:w-[32%] lg:w-[24%]">
+              <Product
+                productTitle={item.title}
+                productImg={item.thumbnail}
+                bdgText={"15%"}
+                productPrice={`$${item.price}`}
+              />
+            </div>
+                 
+              ))
+            }
+           
+            
+           
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default SpecialOffer;
