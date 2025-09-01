3c413e297c82ae5d449eab18d1de10863b0a77c9
abdunnoorfaheem <abdunnoor3030@gmail.com>
2025-09-01 09:05:43 +0600

    shop by category btn category added

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -5,61 +5,98 @@ import Image from "../Image";
 import Logo from "/src/assets/logo.png";
 import Flex from "../Flex";
 import Button from "../Button";
-import { FaSearch,FaUser,FaCaretDown,FaShoppingCart   } from "react-icons/fa";
+import { FaSearch, FaUser, FaCaretDown, FaShoppingCart } from "react-icons/fa";
 import { FaBarsStaggered } from "react-icons/fa6";
 import Heading from "../Heading";
-
+import {useState} from "react";
 
 const Header = () => {
+  let [viewCategory, setViewCategory] = useState(false);
+
+  function handleCategory() {
+    setViewCategory(!viewCategory);
+  }
+
   return (
     <>
       <div className="">
         <Container>
           <Flex className={"justify-between py-[30px]"}>
             <div className="w-[40%]">
-              <Link to={"/"}><Image imgSrc={Logo} /></Link>
-              
+              <Link to={"/"}>
+                <Image imgSrc={Logo} />
+              </Link>
             </div>
             <div className="w-[60%]">
               <ul className="flex gap-x-12 text-[#767676]">
-              <Link to={"/"}>
-                <li className="hover:text-[#262626] hover:font-bold duration-500">Home</li>
-              </Link>
-              <Link to={"/shop"}>
-                <li className="hover:text-[#262626] hover:font-bold duration-500">Shop</li>
-              </Link>
-              <Link to={"/about"}>
-                <li className="hover:text-[#262626] hover:font-bold duration-500">About</li>
-              </Link>
-              <li className="hover:text-[#262626] hover:font-bold duration-500"><Link to={"/contact"}>Contacts</Link></li>
-            <li className="hover:text-[#262626] hover:font-bold duration-500"><Link to={"/journal"}>Journal</Link></li>
-            </ul>
+                <Link to={"/"}>
+                  <li className="hover:text-[#262626] hover:font-bold duration-500">
+                    Home
+                  </li>
+                </Link>
+                <Link to={"/shop"}>
+                  <li className="hover:text-[#262626] hover:font-bold duration-500">
+                    Shop
+                  </li>
+                </Link>
+                <Link to={"/about"}>
+                  <li className="hover:text-[#262626] hover:font-bold duration-500">
+                    About
+                  </li>
+                </Link>
+                <li className="hover:text-[#262626] hover:font-bold duration-500">
+                  <Link to={"/contact"}>Contacts</Link>
+                </li>
+                <li className="hover:text-[#262626] hover:font-bold duration-500">
+                  <Link to={"/journal"}>Journal</Link>
+                </li>
+              </ul>
             </div>
-            
           </Flex>
         </Container>
         <div className="bg-[#F5F5F3] py-[30px]">
           <Container>
             <Flex className={"justify-between"}>
               <div className="flex items-center gap-x-2 text-[14px]">
-                <FaBarsStaggered />
-                <Heading text={"Shop by Category"} tagName={"h4"} className={"text-[#262626]"}/>
+                <FaBarsStaggered  onClick={handleCategory}/>
+                
+                  <Heading
+                    
+                    text={"Shop by Category"}
+                    tagName={"h4"}
+                    className={"text-[#262626]"}
+                  />
+                { viewCategory && ( <div className=" bg-black text-white">
+                <ul>
+                  <li className="border-b-2">beauty</li>
+                  <li className="border-b-2">beauty</li>
+                  <li className="border-b-2">beauty</li>
+                  <li className="border-b-2">beauty</li>
+                  <li className="border-b-2">beauty</li>
+                </ul>
               </div>
+                )}
+              
+              </div>
+                
               <div className="relative">
-                <input type="text" placeholder="Search Products" className="bg-white py-[16px] pl-[21px] w-[600px] outline-0 placeholder:text-[#C4C4C4]" />
-                <FaSearch className="absolute right-3 top-[50%] -translate-[50%]"/>
+                <input
+                  type="text"
+                  placeholder="Search Products"
+                  className="bg-white py-[16px] pl-[21px] w-[600px] outline-0 placeholder:text-[#C4C4C4]"
+                />
+                <FaSearch className="absolute right-3 top-1/2 -translate-1/2" />
               </div>
               <div className="flex items-center gap-x-3">
                 <div className="flex">
                   <FaUser />
-                <FaCaretDown />
+                  <FaCaretDown />
                 </div>
                 <FaShoppingCart />
               </div>
             </Flex>
-              
           </Container>
-         </div>
+        </div>
       </div>
     </>
   );
