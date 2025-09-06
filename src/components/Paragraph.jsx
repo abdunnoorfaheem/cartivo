6363bc465abe8f0354bc7667284a6daa47f59147
abdunnoorfaheem <abdunnoor3030@gmail.com>
2025-09-06 14:31:30 +0600

    paragraph component added division and discrict option added

---
+++ src/components/Paragraph.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Paragraph = ({children}) => {
+  return (
+    <>
+      <p>
+        {children}
+      </p>
+    </>
+  )
+}
+
+export default Paragraph;
--- src/components/pages/SignUp.jsx
+++ src/components/pages/SignUp.jsx
@@ -1,12 +1,13 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Container from "../Container";
 import CommonPages from "../CommonPages";
 
 const SignUp = () => {
   let [allDivisionData, setAllDivisionData] = useState([]);
-  let [allDistrictData,setAllDistrictData]=useState([]);
+  let [allDistrictData, setAllDistrictData] = useState([]);
+  let [selectedDivision, setSelectedDivision] = useState("");
+  let [selectedDistrict, setSelectedDistrict] = useState("");
 
   useEffect(() => {
     async function viewData() {
@@ -15,40 +16,70 @@ const SignUp = () => {
       );
       setAllDivisionData(data.data.divisions);
 
-      let districtData=await axios.get("https://raw.githubusercontent.com/ifahimreza/bangladesh-geojson/master/bd-districts.json");
+      let districtData = await axios.get(
+        "https://raw.githubusercontent.com/ifahimreza/bangladesh-geojson/master/bd-districts.json"
+      );
       setAllDistrictData(districtData.data.districts);
-      
     }
     viewData();
   }, []);
 
+  // নির্বাচিত division অনুযায়ী district filter
+  const filteredDistricts = selectedDivision
+    ? allDistrictData.filter(
+        (district) => district.division_id === selectedDivision
+      )
+    : [];
+
   return (
-    <>
-      <div className="">
-        <Container>
-          <CommonPages title={"Sign up"} description={"Home  >  Sign up"} />
-          <div className="">
-            {allDivisionData.map((item) => (
-              <div className="py-5" key={item.id}>
-               
-               
-               <h1>{item.name}</h1>
-              </div>
-            ))}
+    <div>
+      <Container>
+        <CommonPages title={"Sign up"} description={"Home  >  Sign up"} />
+        <div className="">
+          
+          </div>   
+        <div className="max-w-md mx-auto mt-8 space-y-6">
+          {/* Division Dropdown */}
+          <div>
+            <label className="block mb-2 font-semibold">Select Division</label>
+            <select
+              value={selectedDivision}
+              onChange={(e) => {
+                setSelectedDivision(e.target.value);
+                setSelectedDistrict(""); // নতুন division select করলে district reset হবে
+              }}
+              className="w-full border rounded p-2"
+            >
+              <option value="">-- Select Division --</option>
+              {allDivisionData.map((div) => (
+                <option key={div.id} value={div.id}>
+                  {div.name}
+                </option>
+              ))}
+            </select>
           </div>
-          <div className="">
-            {
-              allDistrictData.map((item)=>(
-                <div className="">
-                  <h3>{item.id}</h3>
-                  <h3>{item.name}</h3>
-                </div>
-              ))
-            }
-          </div>
-        </Container>
-      </div>
-    </>
+
+          {/* District Dropdown */}
+          {selectedDivision && (
+            <div>
+              <label className="block mb-2 font-semibold">Select District</label>
+              <select
+                value={selectedDistrict}
+                onChange={(e) => setSelectedDistrict(e.target.value)}
+                className="w-full border rounded p-2"
+              >
+                <option value="">-- Select District --</option>
+                {filteredDistricts.map((dist) => (
+                  <option key={dist.id} value={dist.id}>
+                    {dist.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+          )}
+        </div>
+      </Container>
+    </div>
   );
 };
 
