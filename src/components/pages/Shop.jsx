f4576069c9710bbb4c54d6c67760bbffbfb1c0dc
abdunnoorfaheem <abdunnoor3030@gmail.com>
2025-09-20 23:19:50 +0600

    pagination added

--- src/components/pages/Shop.jsx
+++ src/components/pages/Shop.jsx
@@ -15,6 +15,40 @@ const Shop = () => {
       
    }
    allShowData();
+
+   let [currentPage,setCurrentPage]=useState(1);
+   let [perPage,setPerPage]=useState(15);
+
+   let lastItemIndex=currentPage * perPage;
+
+   let firstItemIndex=lastItemIndex-perPage;
+
+   let perPageProducts=allData.slice(firstItemIndex,lastItemIndex);
+  //  console.log(perPageProducts);
+    
+
+   let pageNumber=Math.ceil(allData.length/perPage);
+   
+   let numbers =[];
+   for(let i=1;i<=pageNumber;i++){
+       numbers.push(i);
+       
+   }
+  //  console.log(numbers);
+   let handlePrevPage=()=>{
+    if(currentPage > 1){
+      setCurrentPage(currentPage - 1)
+    }
+   }
+   let handleNextPage=()=>{
+    
+      if(currentPage !== pageNumber){
+        setCurrentPage(currentPage + 1);
+      }
+    
+   }
+    
+
   return (
     
     
@@ -23,7 +57,7 @@ const Shop = () => {
         <Container>
           
         <Flex className={"flex-wrap gap-x-2 gap-y-4"}>
-          {allData.map((item)=>(
+          {perPageProducts.map((item)=>(
          <div className="w-[100%] lg:w-[32%] px-[20px] py-[10px] m-auto shadow-2xl">
           <div className="text-center"><img src={item.thumbnail} alt="" /></div>
           <h1 className="text-2xl font-bold">{item.title}</h1>
@@ -33,6 +67,15 @@ const Shop = () => {
          </div>
         ))}
         </Flex>
+        <ul className="flex gap-x-8 py-10 justify-center text-gray-500">
+           <li className="p-3 border-2" onClick={handlePrevPage}>Prev</li>
+          {
+            numbers.map((item)=>(
+              <li className={`p-3 border-2 ${currentPage===item ? "bg-black text-white": ""}`} onClick={()=>setCurrentPage(item)}>{item}</li>
+            ))
+          }
+          <li className="p-3 border-2" onClick={handleNextPage}>Next</li>
+        </ul>
       </Container>
       </div>
     </>
