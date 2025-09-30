3a5d51aab837b38637ecfb45ae0d517aa9a0ba29
abdunnoorfaheem <abdunnoor3030@gmail.com>
2025-09-30 23:36:17 +0600

    aos added bestsellers section and specialoffer section

--- src/components/layouts/Bestsellers.jsx
+++ src/components/layouts/Bestsellers.jsx
@@ -4,9 +4,19 @@ import Product from "../Product";
 import ProductImgFour from "/src/assets/productImgFour.png";
 import Heading from "../Heading";
 import {useState,useEffect} from "react";
-import axios from "axios"
+import axios from "axios";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Bestsellers = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1500, // animation duration (ms)
+      offset: 100, // scroll offset
+      once: false, // একবার animate হবে
+    });
+  }, []);
+  
   let [allData,setAllData]=useState([]);
   
   useEffect(() => {
@@ -27,12 +37,12 @@ const Bestsellers = () => {
           tagName={"h3"}
           className={"md:text-[39px] font-bold text-[#262626]"}
         />
-        <div className="md:flex flex-wrap gap-x-4">
+        <div className="md:flex flex-wrap gap-x-4" >
 
           {
             allData.slice(0,8).map((item)=>(
 
-                <div className="md:w-[48%]  lg:w-[24%] py-3 md:py-4" key={item.id}>
+                <div className="md:w-[48%]  lg:w-[24%] py-3 md:py-4" key={item.id} data-aos="fade-right">
             <Product productTitle={item.title} productImg={item.thumbnail} bdgText={"15%"}  productPrice={  item.price} />
           </div>
             ))
--- src/components/layouts/SpecialOffer.jsx
+++ src/components/layouts/SpecialOffer.jsx
@@ -6,8 +6,17 @@ import SpecialImgTwo from "/src/assets/specialProductTwo.png";
 import Container from "../Container";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const SpecialOffer = () => {
+    useEffect(() => {
+    AOS.init({
+      duration: 2000, // animation duration (ms)
+      offset: 100, // scroll offset
+      once: false, // একবার animate হবে
+    });
+  }, []);
   let [allData, setAllData] = useState([]);
 
   useEffect(() => {
@@ -31,7 +40,7 @@ const SpecialOffer = () => {
             {
               allData.map((item)=>(
                   
-                <div className="md:w-[48%]  lg:w-[24%] py-3 md:py-0" key={item.id}>
+                <div className="md:w-[48%]  lg:w-[24%] py-3 md:py-0" key={item.id} data-aos="fade-left">
               <Product
                 productTitle={item.title}
                 productImg={item.thumbnail}
